d0010bf81c1e2fd3e9f2746f246acbfa8cbb4b64
Sammy Odi <pro.odisammyv@gmail.com>
2024-12-06 19:05:26 +0800

    Second Commit for BLATSY

---
+++ BluetoothAttendanceSystem/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BluetoothAttendanceSystem
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
